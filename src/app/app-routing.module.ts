272e60a3690d993a7e127e52fe5d4040acb0487d
Gabriel Campos <gabrielfcampos2017@gmail.com>
2023-06-04 22:53:11 -0300

    hash router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { HomeComponent } from './components/home/home.component';
 import { XadrezComponent } from './components/xadrez/xadrez.component';
@@ -11,6 +12,7 @@ const routes: Routes = [{ path: '', component: HomeComponent },{ path: 'xadrez',
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppRoutingModule { }
